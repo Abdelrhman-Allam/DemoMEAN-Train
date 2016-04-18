771c37f912075459b1562b1b6a11552b261a4576
abdelrhman.allam <abdelrhman.allam.32@gmail.com>
2016-04-18 22:50:28 +0200

    added

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+	// return html page and bind title
   res.render('index.html', { title: 'Expraess' });
 });
-
+// expose the router
 module.exports = router;
